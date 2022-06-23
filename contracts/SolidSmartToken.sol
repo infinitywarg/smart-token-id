4910e95e38d4cf3defc13c5e0d9f0f94035d8e1f
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-06-23 18:12:07 +0530

    remove extra brackets

--- contracts/SolidSmartToken.sol
+++ contracts/SolidSmartToken.sol
@@ -26,11 +26,14 @@ contract SolidSmartToken {
         pure
         returns (Metadata memory data)
     {
-        data.data1 = address(uint160(((tokenId & ((2**160 - 1) << 96)) >> 96)));
-        data.data2 = uint32(((tokenId & ((2**32 - 1) << 64)) >> 64));
-        data.data3 = uint16(((tokenId & ((2**16 - 1) << 48)) >> 48));
-        data.data4 = uint8(((tokenId & ((2**8 - 1) << 40)) >> 40));
-        data.data5 = bytes3(uint24(((tokenId & ((2**24 - 1) << 16)) >> 16)));
+        data.data1 = address(uint160((tokenId & (((2**160 - 1) << 96) >> 96))));
+        data.data2 = uint32((tokenId & ((2**32 - 1) << 64)) >> 64);
+        data.data3 = uint16((tokenId & ((2**16 - 1) << 48)) >> 48);
+        data.data4 = uint8((tokenId & ((2**8 - 1) << 40)) >> 40);
+        data.data5 = bytes3(uint24((tokenId & (((2**24 - 1) << 16) >> 16))));
         data.data6 = bytes2(uint16(tokenId & (2**16 - 1)));
     }
+
+    // test input: ["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","1111","222","33","0xabcdef","0x9999"]
+    // test output: 41260920106969412157321674113669319076297763250177730572509110742263752792473
 }
